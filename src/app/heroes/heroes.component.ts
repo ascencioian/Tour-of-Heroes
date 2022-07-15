e3a571326efab3d4185723f8b135b993c825cb26
Ian Ascencio <ian.ascencio.mancia@gmail.com>
2022-07-15 15:04:34 -0400

    implemented click events
    added click event to list items to show list item details on click
    added directive ngIf to render details section on click

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -13,8 +13,19 @@ export class HeroesComponent implements OnInit {
   // more than just a name 
   // hero = 'Ian';
 
+  //initiates selectedHero with no value
+  selectedHero?: Hero;
+
+  //function to handle click event
+  onSelected(hero: Hero): void {
+    console.log(hero);
+    this.selectedHero = hero;
+  }
+
+  //imported list of heroes
   heroes = HEROES;
 
+  //single hero object
   hero: Hero = {
     id: 1,
     name: 'Ian'
