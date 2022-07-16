f81befb43fb631d40d2ac27d1015188be00e3579
Ian Ascencio <ian.ascencio.mancia@gmail.com>
2022-07-16 01:00:56 -0400

    implemented component out of hero detail
    abstracted the show hero details feature as its own component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 //puts together components, services to be bootsrapped to project on startup
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
+    HeroDetailComponent,
   ],
   //this is where you put angular modules
   imports: [
